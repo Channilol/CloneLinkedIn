0dde10bfbfe279145ebbf1fe71d7b3ec74a1250d
Francesco Cannizzo <fcannizzo97@gmail.com>
2024-01-22 14:23:21 +0100

    hero section profilo

---
+++ src/components/profilePage/ProfilePage.jsx
@@ -0,0 +1,17 @@
+import './ProfilePage.css'
+import ProfiloTop from './ProfiloTop/ProfiloTop'
+
+const ProfilePage = () => {
+    return (
+        <div className='profilePage'>
+            <div className='profilePageLeft'>
+                <ProfiloTop />
+            </div>
+            <div className='profilePageRight'>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProfilePage
