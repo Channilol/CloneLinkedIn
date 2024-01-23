cb78d8b5e5f0ee34599455515472f7de93f74587
Andrea88Ab <andrea88ab@gmail.com>
2024-01-23 10:48:10 +0100

    Component-Activity-Education

---
+++ src/components/ActivityComponent/ActivityComponent.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import pen from '../Assets/pen_edit_icon_177296.png';
+import './ActivityComponent.css'
+
+const ActivityComponent = () => {
+  const [followers, setFollowers] = useState(0);
+  const [posts, setPosts] = useState([]);
+
+  return (
+    <div className="activity-component">
+      <div className='Activity-post'><h2>Attività</h2>
+      <div className='post-btn'><button className="create-post-button">
+        Crea un post
+      </button>
+      <img src={pen} alt=""  />
+      </div>
+      </div>
+      
+      <div className="followers">
+        {followers} follower
+      </div>
+      {posts.length > 0 ? (
+        <div>
+          <h3>I post che condividi appariranno qui</h3>
+          {}
+        </div>
+      ) : (
+        <div>
+          <h3>Non hai ancora pubblicato nulla</h3>
+          <p>I post che condividi appariranno qui</p>
+        </div>
+      )}
+      <hr style={{ borderColor: 'gray', borderWidth: '0.5px', margin: '20px 0' }} />
+
+      
+      <a href="#show-all" className="show-all-activities">
+        Mostra tutte le attività 
+      </a>
+    </div>
+  );
+};
+
+export default ActivityComponent;
---
+++ src/components/EducationComponent/EducationComponent.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './EducationComponent.css'; 
+import logouni from '../Assets/logouni.png'
+import pen from '../Assets/pen_edit_icon_177296.png'
+import Plus from '../Assets/Plus.png'
+
+const EducationComponent = () => {
+  return (
+    <div className="education-container">
+      <div className="title-section">
+        <h2>Formazione</h2>
+        <div className="icons">
+          <img className='Plus' src={Plus} alt="" />
+          <img  src={pen} alt="" />
+        </div>
+      </div>
+      <div className="education-entry">
+        
+        <div className="education-details">
+          
+        <img className='logo' src={logouni} alt=""  />
+        <div>
+        <h3>Università degli stranieri di Perugia</h3>
+          <p>Diploma Istituto Tecnico e Professionale, Graphic Design</p>
+          <p>2016 - 2020</p>
+
+        </div>
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EducationComponent;
