6f0f3f9608e0a5cb096fe635684467147d5bb348
Fra-margo <margot.f.98@gmail.com>
2024-01-24 12:12:27 +0100

    ulteriori modifiche header

--- src/components/MyHeader/MyHeader.jsx
+++ src/components/MyHeader/MyHeader.jsx
@@ -18,8 +18,8 @@ const MyHeader = () => {
     };
 
     const toggleMobileMenu = () => {
-        setMobileMenuOpen(!isMobileMenuOpen);
-    };
+        setMobileMenuOpen(!isMobileMenuOpen)
+    }
 
     const handleSearchIconClick = () => {
         setIsSearchVisible(!isSearchVisible);
@@ -27,25 +27,25 @@ const MyHeader = () => {
 
     useEffect(() => {
         const handleOutsideClick = (event) => {
-          
-          if (searchRef.current && !searchRef.current.contains(event.target)) {
-            setIsSearchVisible(false);
-          }
+
+            if (searchRef.current && !searchRef.current.contains(event.target)) {
+                setIsSearchVisible(false);
+            }
         };
 
         document.addEventListener('click', handleOutsideClick);
 
         return () => {
-          document.removeEventListener('click', handleOutsideClick);
+            document.removeEventListener('click', handleOutsideClick);
         };
-      }, []);
+    }, []);
 
     return (
         <header className="custom-navbar">
             <svg xmlns="http://www.w3.org/2000/svg" id="logoNavSvg" width="37" height="37" fill="#0a66c2" className="bi bi-linkedin changeColorSvg" viewBox="0 0 16 16">
                 <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
             </svg>
-            <div id="boxSearchNav"  ref={searchRef}>
+            <div id="boxSearchNav" ref={searchRef}>
                 <div className="divIconsNav" onClick={handleSearchIconClick}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="404040" className="bi bi-search changeColorSvg" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
@@ -53,26 +53,26 @@ const MyHeader = () => {
                     <p className="textIconsNav">Search</p>
                 </div>
                 <div className="iconSearchNav">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="404040" className="bi bi-search changeColorSvg" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="#404040" className="bi bi-search changeColorSvg" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                     </svg>
                 </div>
                 <input type="text" className="inputSearchNav" placeholder="Cerca" />
 
                 {isSearchVisible && (
-                <div id="boxSearchMobile">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="404040" className="bi bi-search changeColorSvg" viewBox="0 0 16 16" id="iconSearchMobile">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                    </svg>
-                    <input type="text" id="inputSearchMobile" placeholder="Cerca" />
-                </div>
+                    <div id="boxSearchMobile">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="#404040" className="bi bi-search changeColorSvg" viewBox="0 0 16 16" id="iconSearchMobile">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                        </svg>
+                        <input type="text" id="inputSearchMobile" placeholder="Cerca" />
+                    </div>
                 )}
 
             </div>
 
 
             <div id="boxIconMobile" onClick={toggleMobileMenu}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#404040" className="bi bi-list" id="svgMobileMenu" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#404040" className="bi bi-list" id="svgMobileMenu" viewBox="0 0 16 16" style={{marginRight: "20px"}}>
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
                 </svg>
             </div>
@@ -161,7 +161,7 @@ const MyHeader = () => {
 
                 <div className="containerIconsNav ">
 
-                    <div className="dropdown-icon" onClick={toggleDropdown}>
+                    <div className="dropdown-icon" onClick={toggleDropdown} style={{ marginRight: "15px" }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-person-circle customPositionSvg" viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                             <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
@@ -211,7 +211,7 @@ const MyHeader = () => {
 
 
                 <div className="containerIconsNav" id="borderContainerIconsNav">
-                    <div className="dropdown-icon" onClick={toggleOtherDropdown}>
+                    <div className="dropdown-icon" onClick={toggleOtherDropdown} style={{ paddingLeft: "10px"}}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#404040" className="bi bi-grid-3x3-gap-fill changeColorSvg" viewBox="0 0 16 16">
                             <path d="M1 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zM1 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zM1 12a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1z" />
                         </svg>
