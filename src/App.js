f53b21ecc78f77e0f7c06690a1c26f96ac896103
Francesco Cannizzo <fcannizzo97@gmail.com>
2024-01-25 10:06:45 +0100

    commenti da finire

--- src/App.js
+++ src/App.js
@@ -5,8 +5,26 @@ import MyHeader from './components/MyHeader/MyHeader';
 import Homepage from './components/Homepage/Homepage';
 import Jobspage from './components/JobsPage/Jobspage';
 import JobsPage2 from './components/JobsPage2/JobsPage2';
+import { useDispatch } from 'react-redux';
+import { getUserFetchAction } from './redux/actions';
+import { useEffect } from 'react';
 
 function App() {
+
+  const apiUrl = `https://striveschool-api.herokuapp.com/api/profile/me`
+    const token = {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFlM2VkMzYwMGJlMTAwMTgzYTg2OTgiLCJpYXQiOjE3MDU5MTgxNjMsImV4cCI6MTcwNzEyNzc2M30.7DYncSKPLwIy7aJwIhh6w0OhrQZ4E4_M74Hg7oUY_DE',
+          'Content-Type': 'application/json'
+        },
+      }
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getUserFetchAction(apiUrl, token))
+    },[])
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -14,7 +32,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Homepage/>}/>
           <Route path='/jobspage' element={<Jobspage />} />
-          <Route path='/jobspage2' element={<JobsPage2 />} />
+          <Route path='/jobspage2/:searchquery' element={<JobsPage2 />} />
           <Route path='/profile/:user' element={<ProfilePage/>}/>
         </Routes>
       </div>  
