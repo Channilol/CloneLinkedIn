90107797df591dba611ea0362f4c72be1939825e
Francesco Cannizzo <fcannizzo97@gmail.com>
2024-01-25 15:37:07 +0100

    sezione job quasi finita

--- src/components/JobsPage2/JobCard/JobCard.jsx
+++ src/components/JobsPage2/JobCard/JobCard.jsx
@@ -4,20 +4,23 @@ import JobLogo from '../../Assets/JobLogo.png'
 import inlogo from '../../Assets/inlogo.png'
 import ProfileIcon from '../../Assets/profileIcon.png'
 import CloseIcon from '../../Assets/CloseIcon.png'
+import { useSelector } from 'react-redux';
 
-function JobCard() {
+function JobCard({jobData, click}) {
+  const loggedUser = useSelector((state) => state.user.userFetch)
+
   return (
-    <div className="job-card">
+    <div className="job-card" onClick={click}>
         <div className='centerOptimizer'>
         <div className="job-logo">
-        <img src={JobLogo} alt="" />
+        {jobData.company_logo_url ? (<img src={jobData.company_logo_url} alt='logoCompany' />) : (<img src={JobLogo} alt="logoCompany" />)}    
       </div>
       <div className="job-info">
-        <h2>Progettista elettrico</h2>
-        <h3>Eco Engineering</h3>
-        <p>Cremona, Lombardia, Italia (In sede)</p>
-        <p> <img src={ProfileIcon} alt="" />Il tuo profilo è in linea con questa offerta di lavoro</p>
-        <p>Promosso · <img className='inlogo' src={inlogo} alt="" /> Candidatura semplice</p>
+        <h2>{jobData.title}</h2>
+        <h3>{jobData.category}</h3>
+        <p>{jobData.company_name}</p>
+        <p> <img src={loggedUser.image} alt="" />Il tuo profilo è in linea con questa offerta di lavoro</p>
+        {/* <p>Promosso · <img className='inlogo' src={inlogo} alt="" /> Candidatura semplice</p> */}
       
       </div>
 
