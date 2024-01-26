af35b2c39028ffa2e90049d8464c346f65bea949
Francesco Cannizzo <fcannizzo97@gmail.com>
2024-01-26 16:23:45 +0100

    versione 1.0.0

--- src/components/JobsPage2/JobsHeader/JobsHeader.jsx
+++ src/components/JobsPage2/JobsHeader/JobsHeader.jsx
@@ -174,11 +174,8 @@ const JobsHeader = () => {
                     </svg>
                     <p className="textIconsNav">Notifiche</p>
                 </div>
-                <div className="mobile-menu-item">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#404040" className="bi bi-person-circle changeColorSvg" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                        <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                    </svg>
+                <div className="mobile-menu-item" onClick={() => navigate('/profile/me')}>
+                    {loggedUser ? <img src={loggedUser.image} /> : <p>Loading...</p>}
                     <p className="textIconsNav">Profilo</p>
                 </div>
                 <div className="mobile-menu-item">
